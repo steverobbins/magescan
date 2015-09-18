8796fb554e618ddd9889ec1d05119ae24545dcfe
Steve Robbins <steve@steverobbins.com>
2015-09-18 15:11:20 -0700

    Incrementing version number

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -18,7 +18,7 @@ use MageScan\Command\ScanCommand;
 use MageScan\Command\SelfUpdateCommand;
 use Symfony\Component\Console\Application;
 
-$app = new Application('Mage Scan', '1.8.1');
+$app = new Application('Mage Scan', '1.8.2');
 
 $app->add(new ScanCommand);
 $app->add(new SelfUpdateCommand);
